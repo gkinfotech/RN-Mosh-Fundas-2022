1039d890351642501cc25a35da86ad09ce5ce616
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-09 21:19:56 +0530

    Mosh_Fundas_Initial

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+export default function App() {
+  let a = 1;
+  console.log(a);
+  return (
+    <SafeAreaView>
+      <Text>Hello Gokul</Text>
+    </SafeAreaView>
+  );
+}
