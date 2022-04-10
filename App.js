2bab02e592bd12a1d96c6e1939b4b9e0a1a51257
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 12:55:02 +0530

    RN_Mosh_Fundas_05_Touchables Examples

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
-import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import React from 'react';
 
 export default function App() {
-  let longText =
-    'React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React Native specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.';
-  const handlePress = () => console.log('Text Clicked!');
+  // TouchableHighlight => A wrapper for making views respond properly to touches. On press down, the opacity of the wrapped view is decreased, which allows the underlay color to show through, darkening or tinting the view.
+  //TouchableOpacity => A wrapper for making views respond properly to touches. On press down, the opacity of the wrapped view is decreased, dimming it.
+  //TouchableWithoutFeedback - TouchableWithoutFeedback supports only one child. If you wish to have several child components, wrap them in a View. Importantly, TouchableWithoutFeedback works by cloning its child and applying responder props to it
+  // TouchableNativeFeedback - A wrapper for making views respond properly to touches (Android only). On Android this component uses native state drawable to display touch feedback
+
+  const clickHandler = () => console.log('Touchables Button Pressed!');
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.containerText}>Hello Gokul</Text>
-      <Text
-        style={styles.containerTextPara}
-        numberOfLines={1}
-        onPress={handlePress}>
-        {longText}
-      </Text>
-      <Image source={require('./testimage.jpg')} />
-      <Image
-        blurRadius={5}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 200,
-          uri: 'https://picsum.photos/seed/picsum/200/300',
-        }}
-      />
+      <TouchableWithoutFeedback onPress={clickHandler}>
+        <Image
+          blurRadius={5}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 200,
+            uri: 'https://picsum.photos/seed/picsum/200/300',
+          }}
+        />
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
