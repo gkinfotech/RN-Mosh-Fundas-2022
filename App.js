b5ead65ebc7c31600eb4ec82c09c75c41000ac43
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 13:01:28 +0530

    RN_Mosh_Fundas_01_05_Buttons

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
-import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Button,
+} from 'react-native';
 import React from 'react';
 
 export default function App() {
-  let longText =
-    'React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React Native specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.';
-  const handlePress = () => console.log('Text Clicked!');
+  const handlePress = () => console.log('Button Clicked!');
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.containerText}>Hello Gokul</Text>
-      <Text
-        style={styles.containerTextPara}
-        numberOfLines={1}
-        onPress={handlePress}>
-        {longText}
-      </Text>
-      <Image source={require('./testimage.jpg')} />
-      <Image
-        blurRadius={5}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 200,
-          uri: 'https://picsum.photos/seed/picsum/200/300',
-        }}
-      />
+      <Button title="Click Me!" onPress={handlePress} />
     </SafeAreaView>
   );
 }
@@ -31,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
   },
