48dc48eb0f4b63b2485cc4893e940466d062b97f
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 15:18:47 +0530

    RN_Mosh_Fundas_09_Platform_OS

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import {StyleSheet, SafeAreaView, Button, Alert} from 'react-native';
+import {StyleSheet, SafeAreaView, Button, Alert, Platform} from 'react-native';
 import React from 'react';
 
 export default function App() {
-  //Alert Prompt will work only for IOS
+  //Platfrom OS will help differentiate Styles from Android/IOS
   const handleAlert = () =>
     Alert.alert('Alert Title', 'Alert Text', [
       {text: 'Yes', onPress: () => console.log('Yes Button tapped')},
@@ -10,11 +10,7 @@ export default function App() {
     ]);
   const containerStyle = {backgroundColor: 'orange'};
   return (
-    //StyleSheet validates the input object
-    // We can also declare the separate style like containerStyle (const containerStyle = {backgroundColor: 'orange'};)
-    // The main advantages here if we did any typo using declare technique it wont validate App will run but stylesheet always expect the input and output
-    //Array of styles style={[styles.container,containerStyle]} -> result is combination of two styles Right style(containerStyle) overrides the properties of left style(container)
-    <SafeAreaView style={[styles.container, containerStyle]}>
+    <SafeAreaView style={styles.container}>
       <Button title="Click Me!" onPress={handleAlert} />
     </SafeAreaView>
   );
@@ -22,8 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'tomato',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Platform.OS === 'android' ? 'tomato' : 'orange',
   },
 });
