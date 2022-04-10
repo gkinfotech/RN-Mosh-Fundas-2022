31bad4a20da4c1c942f62eb8b4e714d695a89195
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 16:31:41 +0530

    RN_Mosh_FC_Layout_03_DeviceOrientation

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import {View, Text, StyleSheet, SafeAreaView, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import React from 'react';
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks';
 
 export default function App() {
-  //Dimensions helps to identify the screen size
-  console.log(Dimensions.get('screen'));
+  //UseDimensions hooks helps to identify the Orienttion screen
+  //npm i @react-native-community/hooks
+  // Consider the scenario Portrait mode => 50% screen should occupy if it is in Landscape occupy full screen
+  console.log(useDimensions());
+  console.log(useDeviceOrientation());
+  const {landscape} = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: 'dodgerblue',
-          width: '50%',
-          height: 70,
+          width: '100%',
+          height: landscape ? '100%' : '30%',
         }}></View>
     </SafeAreaView>
   );
