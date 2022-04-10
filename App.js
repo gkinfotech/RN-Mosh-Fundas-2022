f3b83debbac9e35be174684477a9232366ef2de3
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 13:41:51 +0530

    Alert Prompt

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {StyleSheet, SafeAreaView, Button, Alert} from 'react-native';
 import React from 'react';
 
 export default function App() {
+  //Alert Prompt will work only for IOS
   const handleAlert = () =>
     Alert.alert('Alert Title', 'Alert Text', [
       {text: 'Yes', onPress: () => console.log('Yes Button tapped')},
@@ -10,6 +11,14 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Button title="Click Me!" onPress={handleAlert} />
+      <Button
+        title="Input Prompt"
+        onPress={() =>
+          Alert.prompt('Prompt Title', 'Prompt Description', text =>
+            console.log(text),
+          )
+        }
+      />
     </SafeAreaView>
   );
 }
