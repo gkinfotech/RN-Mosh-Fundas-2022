89922efe178e34cf8a6bb192e220c496f56a4b26
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-10 16:04:58 +0530

    RN_Mosh_FC_Layout_02_Dimensions

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
-import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Dimensions} from 'react-native';
 import React from 'react';
 
 export default function App() {
-  let longText =
-    'React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React Native specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.';
-  const handlePress = () => console.log('Text Clicked!');
+  //Dimensions helps to identify the screen size
+  console.log(Dimensions.get('screen'));
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.containerText}>Hello Gokul</Text>
-      <Text
-        style={styles.containerTextPara}
-        numberOfLines={1}
-        onPress={handlePress}>
-        {longText}
-      </Text>
-      <Image source={require('./testimage.jpg')} />
-      <Image
-        blurRadius={5}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 200,
-          uri: 'https://picsum.photos/seed/picsum/200/300',
-        }}
-      />
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: '50%',
+          height: 70,
+        }}></View>
     </SafeAreaView>
   );
 }
@@ -31,16 +19,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerText: {
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  containerTextPara: {
-    fontSize: 16,
-    fontStyle: 'italic',
+    backgroundColor: 'white',
   },
 });
