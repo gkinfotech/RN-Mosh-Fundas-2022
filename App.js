8b38761add40145d2b4f433dc1f3bf10e0102ecc
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-17 01:47:17 +0530

    RN_Mosh_FC_Layout_06_FlexBox_AI_JC_AS

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
 import React from 'react';
 
 export default function App() {
-  // By Default RN Verically aligns the View
-  // By Adding flexDirection: 'row'=> Views are Horizontally aligned
-  // By Adding flexDirection: 'row-reverse'=> Views are Horizontally aligned with left to Right
+  // FlexBox - JustifyContent, alignItems, alignSelf
+  //  justifyContent: space-evenly |space-between | space-around
+  //alignItems:  baseline | center | flex-end | flex-start | stretch
+  // alignSelf: baseline | center | flex-end | flex-start | auto if you want to change particular view alone
   return (
     <View style={styles.container}>
-      <View style={{backgroundColor: 'dodgerblue', width: 100, height: 100}} />
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          //alignSelf: 'center',
+        }}
+      />
       <View style={{backgroundColor: 'gold', width: 100, height: 100}} />
       <View style={{backgroundColor: 'tomato', width: 100, height: 100}} />
     </View>
@@ -18,6 +26,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#ffff',
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'row', // horizontal
+    justifyContent: 'center', // main Axis
+    alignItems: 'stretch', //secondary Axis
   },
 });
