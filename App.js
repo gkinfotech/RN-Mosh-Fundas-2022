a2738f902c59cb5d7bdee4a690116bd2eec70655
GokulaKrishnan E <gokul.krish55@gmail.com>
2022-04-24 01:07:56 +0530

    Vector Icons

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
 import React from 'react';
 
 import AppText from './app/components/AppText';
+import Icon from 'react-native-vector-icons/Fontisto';
 
 export default function App() {
-  //06 Encapsulating the text style
-  // Custom Text Components
+  //07 - React Native Vector Icons
+  // https://www.npmjs.com/package/react-native-vector-icons#icon-component
   return (
     <View style={styles.container}>
       <AppText>I Like React Native</AppText>
+      <Icon name="email" size={40} color="#900"></Icon>
     </View>
   );
 }
